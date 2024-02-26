d548fe84c3b52331d829e6ede30b480486fc1533
Mario-Chavez <marioachavez1@gmail.com>
2024-02-26 10:29:48 -0300

    rawheaders terminado

--- src/auth/decorators/get-rawHeaders.decorator.ts
+++ src/auth/decorators/get-rawHeaders.decorator.ts
@@ -1,8 +1,4 @@
-import {
-  ExecutionContext,
-  InternalServerErrorException,
-  createParamDecorator,
-} from '@nestjs/common';
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 export const RawHeaders = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
