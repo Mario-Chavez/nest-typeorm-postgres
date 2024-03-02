06ce2ab2f808f2c8f3ade564a5db56e3fcc3014c
Mario-Chavez <marioachavez1@gmail.com>
2024-03-02 04:18:13 -0300

    socket message

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,19 +1,42 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
+import { NewMessageDto } from './dto/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wss: Server;
   constructor(private readonly messagesWsService: MessagesWsService) {}
-  handleConnection(client: any, ...args: any[]) {
-    throw new Error('Method not implemented.');
+
+  handleConnection(client: Socket) {
+    this.messagesWsService.registerClient(client);
+    /* emite los clientes conectados */
+    this.wss.emit(
+      'clients-update',
+      this.messagesWsService.getConnectedClient(),
+    );
   }
-  handleDisconnect(client: any) {
-    throw new Error('Method not implemented.');
+
+  handleDisconnect(client: Socket) {
+    this.messagesWsService.removeClient(client.id);
+    /* client desconnect */
+    this.wss.emit(
+      'clients-update',
+      this.messagesWsService.getConnectedClient(),
+    );
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    const clientId = client.id;
+    console.log({ clientId, payload });
   }
 }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+interface ConectedClients {
+  [id: string]: Socket;
+}
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+  private conectedClients: ConectedClients = {};
+
+  registerClient(client: Socket) {
+    this.conectedClients[client.id] = client;
+  }
+
+  removeClient(clientId: string) {
+    delete this.conectedClients[clientId];
+  }
+
+  getConnectedClient(): string[] {
+    return Object.keys(this.conectedClients);
+  }
+}
